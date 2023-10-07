a6dbfcf417c2239662d81b9f94cf5d12e9e96d8f
Babygungle <144519233+Babygungle@users.noreply.github.com>
2023-10-07 20:31:59 +0700

    draft:Blue's work

--- ex-2.js
+++ ex-2.js
@@ -3,4 +3,11 @@ let restaurants = ["KFC", "MOMO PARADISE", "Nabezo"];
 let newRestaurants = [];
 
 // Start coding here
-
+let i = -1; 
+while (i < restaurants.length) {
+    i++;
+    newRestaurants.push("Restaurant Name: " + restaurants[i]);
+    
+}
+newRestaurants.pop();
+console.log(newRestaurants) 
